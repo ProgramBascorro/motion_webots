427912540def507358729d2800ef02bb6d5322b1
dzulfi <mdzulfikri795@gmail.com>
2026-03-03 21:17:05 +0700

    Refactor Webots GC bridge: struct-based HL parsing and dedicated gc_bridge_webots module

--- src/ROBOTIS-OP3-Simulations/op3_webots_ros2/launch/robot_launch.py
+++ src/ROBOTIS-OP3-Simulations/op3_webots_ros2/launch/robot_launch.py
@@ -2,10 +2,7 @@ import os
 import launch
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch_ros.parameter_descriptions import ParameterValue
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from ament_index_python.packages import get_package_share_directory
 
@@ -19,11 +16,6 @@ def generate_launch_description():
     package_dir = get_package_share_directory('op3_webots_ros2')
     gain_file_path_default = package_dir + '/resource/op3_pid_gain_default.yaml'
 
-    # Load robot description (URDF)
-    op3_description_path = FindPackageShare('op3_description')
-    op3_urdf_path = PathJoinSubstitution([op3_description_path, 'urdf', 'robotis_op3.urdf.xacro'])
-    op3_description_content = ParameterValue(Command(['xacro ', op3_urdf_path]), value_type=str)
-
     webots = WebotsLauncher(
         world=os.path.join(package_dir, 'worlds', 'robotis_op3_extern.wbt')
     )
@@ -35,13 +27,29 @@ def generate_launch_description():
         output='screen',
         parameters=[{'gain_file_path': gain_file_path_default}]
     ))
-
-    # robot_state_publisher for TF tree (base -> ... -> cam_link)
     ld.add_action(Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': op3_description_content}],
-        remappings=[('/joint_states', '/robotis_op3/joint_states')],
+        package='op3_webots_ros2',
+        executable='op3_gc_bridge',
+        output='screen',
+        parameters=[{
+            'gc_host': '127.0.0.1',
+            'control_port': 3838,
+            'status_port': 3939,
+            'team_number': 1,
+            'player_number': 1,
+            'rx_period_ms': 20,
+            'tx_period_ms': 500
+        }]
     ))
 
     return ld
+    # return LaunchDegiscription([
+    #     webots,
+    #     my_robot_driver,
+    #     launch.actions.RegisterEventHandler(
+    #         event_handler=launch.event_handlers.OnProcessExit(
+    #             target_action=webots,
+    #             on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+    #         )
+    #     )
+    # ])
